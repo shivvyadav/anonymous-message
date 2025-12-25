50635554617a198f6599db771ff1e42a13c37198
Shiv Yadav <shivyadav0014@gmail.com>
2025-12-25 22:10:11 +0545

    add sending message route

---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,35 @@
+import User from "@/models/User";
+import connectDB from "@/lib/dbConnect";
+import {Imessage} from "@/models/User";
+
+export async function POST(request: Request) {
+  await connectDB();
+  const {username, content} = await request.json();
+
+  try {
+    const user = await User.findOne({username}).exec();
+
+    if (!user) {
+      return Response.json({message: "User not found", success: false}, {status: 404});
+    }
+
+    // Check if the user is accepting messages
+    if (!user.isAcceptingMessage) {
+      return Response.json(
+        {message: "User is not accepting messages", success: false},
+        {status: 403}
+      );
+    }
+
+    const newMessage = {content, createdAt: new Date()};
+
+    // Push the new message to the user's messages array
+    user.messages.push(newMessage as Imessage);
+    await user.save();
+
+    return Response.json({message: "Message sent successfully", success: true}, {status: 201});
+  } catch (error) {
+    console.error("Error adding message:", error);
+    return Response.json({message: "Internal server error", success: false}, {status: 500});
+  }
+}
